3f4eb68aaa8c0257b8f663a463d04f4289860767
jeongyoni <jungyoni990604@gmail.com>
2025-04-01 00:07:00 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home'; // 첫 페이지 컴포넌트
+import Home from './features/home/Home'; // 올바른 경로로 수정
 import KeyboardExperience from './components/KeyboardExperience'; // 키보드 체험 페이지 컴포넌트
 
 const App = () => {
