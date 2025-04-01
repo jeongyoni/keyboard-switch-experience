480416a0ee0104f523a41e943249afb88d45a48e
jeongyoni <jungyoni990604@gmail.com>
2025-04-01 23:53:58 +0900

    Update KeyboardExperience.js

--- src/features/keyboardExperience/KeyboardExperience.js
+++ src/features/keyboardExperience/KeyboardExperience.js
@@ -6,7 +6,6 @@ const KeyboardExperience = () => {
     <div>
       <h1>키보드 체험하기</h1>
       <p>이곳에서 Groovestone 키보드를 체험할 수 있습니다.</p>
-      {/* 키보드 시뮬레이터 렌더링 */}
       <KeySimulator />
     </div>
   );
