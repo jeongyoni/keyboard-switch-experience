06d355a594044aa22e13d7bd689560a901b4d800
jeongyoni <jungyoni990604@gmail.com>
2025-04-01 09:41:38 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-// vite.config.js
-export default {
-  base: '/',
-  server: {
-    historyApiFallback: true,  // This will handle all route requests and serve the index.html
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './', // 상대 경로로 설정
+  build: {
+    outDir: 'dist', // 빌드 결과물 디렉토리
+    assetsDir: 'assets', // 번들링된 파일이 저장될 디렉토리
   },
-};
+  server: {
+    historyApiFallback: true, // SPA 라우팅 지원
+  },
+});
