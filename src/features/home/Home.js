e347513653516a36152505ab0dd5c9b39ee27acf
jeongyoni <jungyoni990604@gmail.com>
2025-04-01 23:53:21 +0900

    Update Home.js

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -5,8 +5,8 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home-container"> {/* className 추가 */}
-      <h1>Groovestone 키보드 체험</h1>
-      <p>여기에서 다양한 그루브스톤 키보드를 직접 체험해 보세요!</p>
+      <h1>Groovestone </h1>
+      <p>여기에서 다양한 그루브스톤 축을 직접 체험해 보세요!</p>
       <Link to="/keyboard-experience">키보드 체험하기</Link> {/* a 태그 대신 Link 사용 */}
     </div>
   );
